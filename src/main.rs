0361878c7141a75cb6b5a1edf252049aff2e60dc
Tuan Bach Van <bachvtuan@gmail.com>
2024-12-12 14:25:30 +0700

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -26,12 +26,12 @@ fn main() {
     if let Some(year) = year_arg {
         // Calculate and display file sizes by month for the specified year, per folder
         for folder_path in &folder_paths {
-            match calculate_file_sizes_by_month(folder_path, year) {
-                Ok(size_by_month) => {
+            match calculate_file_sizes(folder_path, Some(year)) {
+                Ok(size_by_times) => {
                     println!("\nFile sizes for year {} in folder '{}':", year, folder_path);
 
                     // Sort months in ascending order
-                    let mut sorted_months: Vec<_> = size_by_month.into_iter().collect();
+                    let mut sorted_months: Vec<_> = size_by_times.into_iter().collect();
                     sorted_months.sort_by_key(|&(month, _)| month);
 
                     for (month, size_bytes) in sorted_months {
@@ -45,7 +45,7 @@ fn main() {
     } else {
         // Calculate and display file sizes by year, per folder
         for folder_path in &folder_paths {
-            match calculate_file_sizes_by_year(folder_path) {
+            match calculate_file_sizes(folder_path, None) {
                 Ok(size_by_year) => {
                     println!("\nFile sizes by year in folder '{}':", folder_path);
                     let threshold = 100 * 1_048_576; // 100 MB in bytes
@@ -66,8 +66,9 @@ fn main() {
     }
 }
 
-fn calculate_file_sizes_by_year(folder_path: &str) -> Result<HashMap<i32, u64>, Box<dyn std::error::Error>> {
-    let size_by_year = Mutex::new(HashMap::new());
+
+fn calculate_file_sizes(folder_path: &str, year: Option<i32>) -> Result<HashMap<i32, u64>, Box<dyn std::error::Error>> {
+    let size_by_times = Mutex::new(HashMap::new());
 
     WalkDir::new(folder_path)
         .into_iter()
@@ -79,43 +80,19 @@ fn calculate_file_sizes_by_year(folder_path: &str) -> Result<HashMap<i32, u64>, 
                 let size = metadata.len();
 
                 if let Ok(modified) = metadata.modified() {
+                    
                     let datetime: DateTime<Utc> = modified.into();
-                    let year = datetime.year();
-
-                    // Accumulate sizes by year in a thread-safe way
-                    let mut size_by_year = size_by_year.lock().unwrap();
-                    *size_by_year.entry(year).or_insert(0) += size;
-                }
-            }
-        });
-
-    Ok(size_by_year.into_inner().unwrap())
-}
-
-fn calculate_file_sizes_by_month(folder_path: &str, year: i32) -> Result<HashMap<u32, u64>, Box<dyn std::error::Error>> {
-    let size_by_month = Mutex::new(HashMap::new());
-
-    WalkDir::new(folder_path)
-        .into_iter()
-        .par_bridge()
-        .filter_map(Result::ok)
-        .filter(|entry| entry.path().is_file())
-        .for_each(|entry| {
-            if let Ok(metadata) = fs::metadata(entry.path()) {
-                let size = metadata.len();
-
-                if let Ok(modified) = metadata.modified() {
-                    let datetime: DateTime<Utc> = modified.into();
-                    if datetime.year() == year {
-                        let month = datetime.month();
-
-                        // Accumulate sizes by month in a thread-safe way
-                        let mut size_by_month = size_by_month.lock().unwrap();
-                        *size_by_month.entry(month).or_insert(0) += size;
+                    let m_year: i32 = datetime.year();
+                    let mut key: i32 = m_year;
+                    if Some(m_year) == year {
+                        key = datetime.month() as i32;
                     }
+                    // Accumulate sizes by month in a thread-safe way
+                    let mut size_by_times = size_by_times.lock().unwrap();
+                    *size_by_times.entry(key).or_insert(0) += size;
                 }
             }
         });
 
-    Ok(size_by_month.into_inner().unwrap())
+    Ok(size_by_times.into_inner().unwrap())
 }
